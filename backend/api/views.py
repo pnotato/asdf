2f090f65fce0603a7385e127d9a693b366a54086
Nicholas Chan <nicholasch.an@outlook.com>
2024-11-09 12:07:08 -0800

    temp django save

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,1 +1,17 @@
-from rest_framework.response import Response
+from rest_framework.response import Response # takes in any python/serialized data to JSON
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET'])
+def getData(request):
+    person = {
+        "name": "John Doe",
+        "age": 29,
+        "city": "New York"
+    }
+    return Response(person)
+
+@api_view(['POST'])
+def testData(request):
+    print("Received POST data:", request.data)
+    return Response({"message": "Data received successfully"})
