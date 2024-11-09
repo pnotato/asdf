91eb087027edf1643fd0124bb49dbb397f522e42
Jordan-SFU <jsm36@sfu.ca>
2024-11-09 13:23:02 -0800

    base home screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css';
 import Debate from './Debate';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Debate />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/debate" element={<Debate/>} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <main>
+            <h1>Talk Tuah</h1>
+            <button onClick={() => window.location.href = '/debate'}>Talk Tuah</button>
+        </main>
+    )
+}
+
+export default Home;
